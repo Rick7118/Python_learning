90dcef2756ab5a2078170e7e0df0697a6f83bed3
Rick <155312472+Rick7118@users.noreply.github.com>
2025-02-07 03:19:46 +0530

    Add files via upload
    what i already knew...pretty embarrassing i know

---
+++ variables.py
@@ -0,0 +1,18 @@
+a=20
+print(a) #this is an integer
+
+b=30.4
+print(b) #this is a floating point
+
+c="hello everyone"
+print(c) #this is a string
+
+d=False
+print(d) #this is a boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#type() function gives you the type of the variable
