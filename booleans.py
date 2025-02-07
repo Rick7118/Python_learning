ed05f10399c26d2e4d7fa385e00ab0dada1d6f31
Rick <155312472+Rick7118@users.noreply.github.com>
2025-02-07 05:36:29 +0530

    Add files via upload
    kinda got demotivated ,hence the hour long delay

---
+++ booleans.py
@@ -0,0 +1,10 @@
+x = True
+y = False
+
+print(type(x))
+print(type(y))
+
+num1=20
+num2=21
+
+print(num1>num2)
---
+++ typecasting.py
@@ -0,0 +1,20 @@
+#process of converting one data type into another data type
+
+float_num=3.14
+
+int_num=int(float_num)
+print(type(int_num))
+
+#similarly you can convert an int to a float using float()
+#most of the times it basically adds .0 at the end to convert it to a decimal number
+
+
+#str()
+num=40
+str_num=str(num)
+print(type(str_num))
+
+#bool()
+num1=0
+bool_value=bool(num)
+print(type(bool_value))
